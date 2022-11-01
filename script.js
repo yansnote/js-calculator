e184058a2adab029aae73e3d9671a5c7842f49c0
Yan Naing <tannaing@gmail.com>
2022-11-01 15:41:34 +0630

    Numbers are now shown via switch statement

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ var dispaly = document.getElementById('display')
 
 document.querySelectorAll('.btn').forEach(elem => {
   elem.addEventListener('click', event => {
-    dispaly.value += elem.dataset.value
+    
+    switch (elem.dataset.type) {
+      case 'number':
+        dispaly.value = parseFloat(display.value + elem.dataset.value)
+        break;
+    
+      default:
+        break;
+    }
+
   })
 })
